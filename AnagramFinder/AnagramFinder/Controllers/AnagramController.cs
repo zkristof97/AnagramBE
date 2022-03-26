ece3f5443db2ae238aee48dcf3c368ea36750534
Kristóf <z.kristof97@gmail.com>
2022-03-26 20:20:36 +0100

    first commit

---
+++ AnagramFinder/AnagramFinder/Controllers/AnagramController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnagramFinder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnagramController : ControllerBase
+    {
+
+    }
+}
